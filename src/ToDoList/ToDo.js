667ce41b5c4647f230c5d6cd3b2895405c699fd2
Kacper Swietlaga <kacperres@gmail.com>
2018-08-01 16:05:22 +0200

    cleared App js

--- src/ToDoList/ToDo.js
+++ src/ToDoList/ToDo.js
@@ -1,1 +1,26 @@
 import React from 'react'
+
+
+class ToDo extends React.Component {
+    state = {[
+        {isCompeleted: false, text: 'umyj dupe', key: '585'},
+    ],
+    newTaskText: ''
+
+    }
+
+
+
+    
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default ToDo 
