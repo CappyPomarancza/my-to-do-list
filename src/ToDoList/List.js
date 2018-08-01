c15d12a0a1b5f66476059eaeae0c96fca3700bef
Kacper Swietlaga <kacperres@gmail.com>
2018-08-01 17:28:16 +0200

    added List component

---
+++ src/ToDoList/List.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Task from './Task'
+
+const List = (props) => (
+    <div>
+        {
+            props.taskList.map(
+                (task, index) => (
+                    <Task 
+                        task={task}
+                        key={task.key}
+                        />
+                )
+            )
+        }
+    </div>
+)
