ca7d98fa7de6b9d9eabd17d74661d0d4003eab55
Kacper Swietlaga <kacperres@gmail.com>
2018-08-05 12:46:06 +0200

    added save in localStorage

--- src/ToDoList/ToDo.js
+++ src/ToDoList/ToDo.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Forms from './Forms'
 import List from './List'
+import Search from './Search';
 
 
 class ToDo extends React.Component {
     state = {
         tasks: [
-           // { isCompeleted: false, text: 'umyj zęby(test)', key: '585' },
+            // { isCompeleted: false, text: 'umyj zęby(test)', key: '585' },
             //{ isCompeleted: false, text: 'umyj zęby(test2)', key: '505' }
         ],
         newTaskText: ''
@@ -40,10 +41,21 @@ class ToDo extends React.Component {
         localStorage.setItem('Cappy-app-my-todo-list', JSON.stringify(this.state))
     }
 
+    searchPhraseChangeHandler = (event) => {
+        this.setState({
+            searchPhrase: event.target.value
+        })
+    }
+
 
     render() {
+        this.componentWillMount()
         return (
             <div>
+                <Search
+                    //dupa={this.state.searchPharse}
+                    mojaNazwaPropsa={this.searchPhraseChangeHandler}
+                />
                 <Forms
                     newTaskText={this.state.newTaskText}
                     onNewTaskTextChanged={this.onNewTaskTextChanged}
